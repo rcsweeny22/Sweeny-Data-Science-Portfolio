787914b019fcff3b346d1475b6e0829ed8634d8c
Rachel Sweeny <rcsweeny22@gmail.com>
2025-05-07 11:10:56 -0400

    Update ML_U_App.py

--- MLUnsupervisedApp/ML_U_App.py
+++ MLUnsupervisedApp/ML_U_App.py
@@ -32,6 +32,14 @@ with tab1:
 ### Download or Upload DataSet ###
 
 with tab2:
+    def preprocess_dataset(df, dropna_columns=None):
+        if dropna_columns:
+            df.dropna(subset=dropna_columns, inplace=True)
+        non_numeric_cols = [col for col in df.columns if not pd.api.types.is_numeric_dtype(df[col])]
+        if non_numeric_cols:
+            df = pd.get_dummies(df, columns=non_numeric_cols, drop_first=True)
+        return df
+    
     def load_and_preprocess_data(): #defining: loading and preprocessing data
         st.markdown("""
                     ### Important Instructions:
@@ -45,27 +53,19 @@ with tab2:
             Seaborn_dataset = st.selectbox("Choose a Seaborn dataset:", dataset_names) # Streamlit widget
             if Seaborn_dataset == 'titanic':# if they choose to look at Seaborn dataset, load it to df
                 df = sns.load_dataset(Seaborn_dataset) # defining df by Seaborn dataset
-                 # Remove rows with missing 'age' values
-                df.dropna(subset=['age'], inplace=True)
-                # One-hot encode the 'sex' column (drop first category)
-                df = pd.get_dummies(df, columns=['sex'], drop_first=True)
+                df = preprocess_dataset(df, dropna_columns=["age"]) # Remove rows with missing 'age' values
             if Seaborn_dataset == 'penguins':
                 df = sns.load_dataset(Seaborn_dataset) # defining df by Seaborn dataset
-                df.dropna(subset = ['sex', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
-                df = pd.get_dummies(df, columns=['sex'], drop_first=True)
+                df = preprocess_dataset(df, dropna_columns=['sex', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
             if Seaborn_dataset == 'taxis':
                 df = sns.load_dataset(Seaborn_dataset) # defining df by Seaborn dataset
-                df.dropna(subset = ['payment', 'pickup_zone', 'dropoff_zone', 'pickup_borough', 'dropoff_borough'])
-                df = pd.get_dummies(df, columns=['payment', 'pickup_zone', 'dropoff_zone', 'pickup_borough', 'dropoff_borough'], drop_first=True)
+                df = preprocess_dataset(df, dropna_columns=['payment', 'pickup_zone', 'dropoff_zone', 'pickup_borough', 'dropoff_borough'])
         else:
             user_file = st.file_uploader('Upload a csv file', type = 'csv') # use streamlit widget for uploading files - set to only accepting csv
             if user_file: # if the user uploads a file then that will be set as the df variable
                 df = pd.read_csv(user_file) # define df by user csv.file if they choose to upload one
                 df.dropna(inplace=True)
-
-        non_numeric_cols = [col for col in df.columns if not pd.api.types.is_numeric_dtype(df[col])]
-        if non_numeric_cols:
-            df = pd.get_dummies(df, columns=non_numeric_cols, drop_first=True)
+                df=preprocess_dataset(df)
 
         # Display dataset
         if df is not None: # if the df is defined by Seaborn data or user data
